28d47608f3b4bf5d1c11a120ea1395c6b3cf3ff0
7pranit <pranitkonde007@gmail.com>
2024-08-10 12:23:37 +0530

    This is my simple test commit

---
+++ MyTest/src/com/sample/SampleDemo.java
@@ -0,0 +1,13 @@
+package com.sample;
+
+public class SampleDemo {
+	
+	public static void main(String[] args) {
+	
+		 int a = 20;
+		 int b = 45;
+		 int c = a + b;
+		 
+		 System.out.println("Addition: "+c);
+}
+}
